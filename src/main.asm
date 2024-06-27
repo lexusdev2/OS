faf9cac7b4c44806bf7992aa5395189eb36349f8
lexusdev2 <ritchie.sotto.agan@gmail.com>
2024-06-27 06:26:08 -0700

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,8 +1,32 @@
 org 0x7C000
 bits 16
 
+start:
+    jmp main
+
+puts:
+    push s1
+    push ax
+
+loop:
+    lodsb
+    or al, al 
+    jz .done
+    jmp .loop
+
+.done:
+    pop ax
+    pop si
+    ret
+
 main:
     hlt
+    mov ax, 0
+    mov ds, ax
+    mov os, ax
+
+    mov ss, ax
+    mov sp, 0x7C000
     
 .halt:
     .jmp .halt
